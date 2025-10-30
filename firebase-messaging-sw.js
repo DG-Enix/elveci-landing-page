c5bcbbf0b9da95f76d4b254e5189b18284c79a94
Jorge Malagon <jorgemalagon.z@gmail.com>
2025-10-29 22:20:14 -0500

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,29 +1,27 @@
-// src/firebase-messaging.ts
+// public/firebase-messaging-sw.js
 importScripts(
-  "https://www.gstatic.com/firebasejs/10.12.1/firebase-app-compat.js"
+  "https://www.gstatic.com/firebasejs/10.14.0/firebase-app-compat.js"
 );
 importScripts(
-  "https://www.gstatic.com/firebasejs/10.12.1/firebase-messaging-compat.js"
+  "https://www.gstatic.com/firebasejs/10.14.0/firebase-messaging-compat.js"
 );
 
-const firebaseConfig = {
+firebase.initializeApp({
   apiKey: "AIzaSyDZFflau4i1D-bRCU0VYtCQacEyfmbOMEY",
   authDomain: "elvecimarket-push-svc.firebaseapp.com",
   projectId: "elvecimarket-push-svc",
   storageBucket: "elvecimarket-push-svc.firebasestorage.app",
-  messagingSenderId: "your_s516304000089ender_id",
+  messagingSenderId: "516304000089",
   appId: "1:516304000089:web:f9583a8a2967858a4acd48",
-};
+});
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  console.log("Received background message ", payload);
-  const notificationTitle = payload.notification?.title || "Notification";
-  const notificationOptions = {
-    body: payload.notification?.body || "",
-    icon: "/icon.png",
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
+  console.log("📨 Received background message: ", payload);
+  const { title, body, icon } = payload.notification;
+  self.registration.showNotification(title, {
+    body,
+    icon: icon || "/vite.svg",
+  });
 });
